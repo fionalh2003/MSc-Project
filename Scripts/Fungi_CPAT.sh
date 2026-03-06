a2e92e8004cda48044a2e1cf3f85aa4aacd87eb7
fionalh2003 <fhutc029@uottawa.ca>
2026-03-06 15:49:21 -0500

    CPAT.sh

--- Scripts/Fungi_CPAT.sh
+++ Scripts/Fungi_CPAT.sh
@@ -48,3 +48,28 @@ cut -f1 07-RhizophagusPredicted_lncRNA_IDs.txt | tail -n +2 > 08-RhizophagusPred
 # Extract FASTA sequences for first AMF
 seqkit grep -f 08-RhizophagusPredicted_lncRNA_IDs.txt 04-RhizophagusIrregularisRNA.fasta > 09-RhizophagusPredicted_lncRNA_IDs.fasta
 
+# Generate hexamer table with refined AMF predictions
+make_hexamer_tab -c 10-RhizophagusIrregularisCDS.fasta -n 09-RhizophagusPredicted_lncRNA_IDs.fasta >11-RhizophagusIrregularisNewHexamerTable.tsv
+
+# Train logistic model with refined AMF predictions
+make_logitModel -x 33-RhizophagusIrregularisNewHexamerTable.tsv -c 04-RhizophagusIrregularisRNA.fasta -n 09-RhizophagusPredicted_lncRNA_IDs.fasta -o 55-RhizophagusIrregularisNew
+
+# Run final  CPAT 00-cpat.sh
+for i in 4401 A1 B3 C2 DAOM A4 A5 G1 SL1
+do
+
+cpat -x 33-RhizophagusIrregularisNewHexamerTable.tsv \
+-d 55-RhizophagusIrregularisNew.logit.RData --top-orf=5 \
+-g 01-$i-illumina-filtered-simple.fasta -o 02-$i-cpat-lnRNA-pred
+
+wait
+
+awk 'NR>1 && $NF <= 0.54 {print $1}' 02-$i-cpat-lnRNA-pred.ORF_prob.best.tsv > 03-$i-cpat-lnRNA-pred_IDs.txt 
+
+wait
+
+seqkit grep -n -f 03-$i-cpat-lnRNA-pred_IDs.txt \
+01-$i-illumina-filtered-simple.fasta \
+> 05-$i-cpat-lnRNA-pred_IDs.fasta
+
+done
