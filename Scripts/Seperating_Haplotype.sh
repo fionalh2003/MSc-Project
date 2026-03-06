692c25aefa06556dd32e12cbaeb48a0e776ba460
fionalh2003 <fhutc029@uottawa.ca>
2026-03-06 15:12:15 -0500

    Update Seperating_Haplotype.sh

--- Scripts/Seperating_Haplotype.sh
+++ Scripts/Seperating_Haplotype.sh
@@ -1,14 +1,11 @@
 for i in A4 A5 G1 SL1
 do
 
-awk 'NR>1 {print $1}' 02-$i-cpat-lnRNA-pred.ORF_prob.best.tsv | \
-grep -wFf - 03-$i-mapped_classification.filtered_lite_classification.txt | \
-awk '{OFS="\t"} $2 ~ /Hap1/ {print $1, $2, $7}' \
-> 44-$i-Hap1.txt
+grep -wFf 03-$i-cpat-lnRNA-pred_IDs.txt 03-$i-mapped_classification.filtered_lite_classification.txt | awk '{OFS="\t"} $2 ~ /Hap1/ {print $1, $2, $7}' > 33-$i-lncRNA-Hap1.txt
 
-awk 'NR>1 {print $1}' 02-$i-cpat-lnRNA-pred.ORF_prob.best.tsv | \
-grep -wFf - 03-$i-mapped_classification.filtered_lite_classification.txt | \
-awk '{OFS="\t"} $2 ~ /Hap2/ {print $1, $2, $7}' \
-> 44-$i-Hap2.txt
+wait
+
+grep -wFf 03-$i-cpat-lnRNA-pred_IDs.txt 03-$i-mapped_classification.filtered_lite_classification.txt | awk '{OFS="\t"} $2 ~ /Hap2/ {print $1, $2, $7}' > 33-$i-lncRNA-Hap2.txt
 
 done
+
