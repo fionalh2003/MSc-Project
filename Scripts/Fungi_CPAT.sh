fe70681c5e52c1f06461ac61ee3e744f2a93873b
fionalh2003 <fhutc029@uottawa.ca>
2026-03-06 15:35:12 -0500

    Update Fungi_CPAT.sh

--- Scripts/Fungi_CPAT.sh
+++ Scripts/Fungi_CPAT.sh
@@ -25,9 +25,26 @@ awk 'NR>1 && $NF <= 0.54 {print $1}' Rhizophagus_CPAT.ORF_prob.best.tsv \
 cut -f1 06-RhizophagusPredicted_lncRNA.tsv | tail -n +2 \
 > 07-RhizophagusPredicted_lncRNA_IDs.txt
 
-# Extract FASTA sequences
+# Extract FASTA sequences from Candida prediction
 seqkit grep -f 07-RhizophagusPredicted_lncRNA_IDs.txt \
 04-RhizophagusIrregularisRNA.fasta \
 > 08-RhizophagusPredicted_lncRNA_IDs.fasta
 
-# 
+# Generate hexamer table using first predicted AMF
+make_hexamer_tab -c 01-RhizophagusIrregularisCDS.fasta -n 02-RhizophagusIrregularisCandidalncRNA.fasta >03-RhizophagusIrregularisHexamerTable.tsv
+
+# Train logistic model for AMF
+make_logitModel -x 03-RhizophagusIrregularisHexamerTable.tsv -c 04-RhizophagusIrregularisRNA.fasta -n 02-RhizophagusIrregularisCandidalncRNA.fasta -o 05-RhizophagusIrregularis
+
+# Run CPAT with first AMF 
+cpat.py   -g 04-RhizophagusIrregularisRNA.fasta   -d 05-RhizophagusIrregularis.logit.RData   -x 03-RhizophagusIrregularisHexamerTable.tsv   -o 06-Rhizophagus_CPAT
+
+# Filter predicted lncRNAs
+awk 'NR>1 && $NF <= 0.54 {print $1}' 06-Rhizophagus_CPAT.ORF_prob.best.tsv > 07-RhizophagusPredicted_lncRNA_IDs.txt 
+
+# Extract IDs
+cut -f1 07-RhizophagusPredicted_lncRNA_IDs.txt | tail -n +2 > 08-RhizophagusPredicted_lncRNA_IDs.txt
+
+# Extract FASTA sequences for first AMF
+seqkit grep -f 08-RhizophagusPredicted_lncRNA_IDs.txt 04-RhizophagusIrregularisRNA.fasta > 09-RhizophagusPredicted_lncRNA_IDs.fasta
+
