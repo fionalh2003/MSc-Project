70479de0bf0d99014c9c59952eb7bb884e7d2d11
fionalh2003 <fhutc029@uottawa.ca>
2026-03-06 14:48:57 -0500

    Create Fungi_CPAT.sh

---
+++ Scripts/Fungi_CPAT.sh
@@ -0,0 +1,31 @@
+# Generate hexamer table
+make_hexamer_tab -c 01-CandidaAlbicansCDS.fasta \
+-n 02-CandidaAlbicanslncRNA.fasta \
+> 03-CandidaAlbicansHexamerTable.tsv
+
+# Train logistic model
+make_logitModel \
+-x 03-CandidaAlbicansHexamerTable.tsv \
+-c 04-CandidaAlbicansRNA.fasta \
+-n 02-CandidaAlbicanslncRNA.fasta \
+-o 05-CandidaAlbicans
+
+# Run CPAT
+cpat.py \
+-g 04-RhizophagusIrregularisRNA.fasta \
+-d 05-CandidaAlbicans.logit.RData \
+-x 03-CandidaAlbicansHexamerTable.tsv \
+-o Rhizophagus_CPAT
+
+# Filter predicted lncRNAs
+awk 'NR>1 && $NF <= 0.54 {print $1}' Rhizophagus_CPAT.ORF_prob.best.tsv \
+> 06-RhizophagusIrregularisCandidaPredicted_lncRNAIDs.txt
+
+# Extract IDs
+cut -f1 06-RhizophagusPredicted_lncRNA.tsv | tail -n +2 \
+> 07-RhizophagusPredicted_lncRNA_IDs.txt
+
+# Extract FASTA sequences
+seqkit grep -f 07-RhizophagusPredicted_lncRNA_IDs.txt \
+04-RhizophagusIrregularisRNA.fasta \
+> 08-RhizophagusPredicted_lncRNA_IDs.fasta
