340b704c8390fd2daabbb39fffd3dd66738d374e
fionalh2003 <fhutc029@uottawa.ca>
2026-03-20 13:09:20 -0400

    Create Expression.sh

---
+++ Scripts/Expression.sh
@@ -0,0 +1,8 @@
+# filter TPM to only include lncRNA
+awk 'NR==FNR{a[$1]; next} FNR==1 || $1 in a' transcripts_to_keep.tx 20-$i-super-table-iso-class-TPM.txt > 05-$i-lncRNA-TPM.txt
+
+# only include transcript name and TPM for each host
+cut -f1,8,9,10 05-$i-lncRNA-TPM.txt > 06-$i-TPM.txt
+
+# Only include values where expression is at least one TPM in one of the 3 hosts
+awk 'NR==1 || $2>=1 || $3>=1 || $4>=1' 06-$i-TPM.txt > 07-$i-FilteredTPM.txt
