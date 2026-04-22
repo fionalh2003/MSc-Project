2324b023ff3bbf80b880eef454f6293c95edf57d
fionalh2003 <fhutc029@uottawa.ca>
2026-04-22 13:58:24 -0400

    Update GffCompare.sh

--- Scripts/GffCompare.sh
+++ Scripts/GffCompare.sh
@@ -13,35 +13,31 @@ gffcompare -r 02-${i}-gene-ann-mod.gtf -o 06-${i}-GFFCompare 01-$i-lncRNA-Mapped
 done
 
 # Filter for only lncRNA codes
-for i in A4 A5 G1 SL1
+for i in 4401 A1 A4 A5 B3 C2 DAOM G1 SL1
 do
-awk '$0 ~ /class_code "[uixjop]"/' 04-$i-GFFCompare.annotated.gtf >05-$i-lncRNA_candidates.gtf
+awk '$0 ~ /class_code "[uix]"/' 06-$i-GFFCompare.annotated.gtf >07-$i-lncRNA_candidates.gtf
 done
 
 # Just transcript ID and code
-for i in A4 A5 G1 SL1
+for i in 4401 A4 A5 B3 C2 DAOM G1 SL1
 do
 awk '$0 ~ /class_code "[a-z]"/ {
   match($0, /transcript_id "([^"]+)"/, t);
   match($0, /class_code "([^"]+)"/, c);
   print t[1] "\t" c[1]
-}' 05-$i-lncRNA_candidates.gtf > 06-$i-lncRNA_ClassCodes.txt
+}' 07-$i-lncRNA_candidates.gtf > 08-$i-lncRNA_ClassCodes.txt
 wait
-sort -u 06-$i-lncRNA_ClassCodes.txt > 06-$i-lncRNA_UniqueClassCodes.txt
-done
-
-# Make a list of just transcript IDs
-for i in A4 A5 G1 SL1
-do
-awk '{print $1}' 07-$i-lncRNA_UniqueClassCodes.txt > 08-$i-GffCompare-lncRNA.txt
+sort -u 08-$i-lncRNA_ClassCodes.txt > 08-$i-lncRNA_UniqueClassCodes.txt
+wait
+awk '{print $1}' 08-$i-lncRNA_UniqueClassCodes.txt > 09-$i-GffCompare-lncRNA.txt
 done
 
 # Seperate by haplotype
 for i in A4 A5 G1 SL1
 do
-grep -wFf 08-$i-GffCompare-lncRNA.txt 03-$i-mapped_classification.filtered_lite_classification.txt | awk '{OFS="\t"} $2 ~ /Hap1/ {print $1, $2, $7}' > 09-$i-lncRNA-Hap1.txt
+grep -wFf 09-$i-GffCompare-lncRNA.txt 03-$i-mapped_classification.filtered_lite_classification.txt | awk '{OFS="\t"} $2 ~ /Hap1/ {print $1, $2, $7}' > 09-$i-lncRNA-Hap1.txt
 wait
-grep -wFf 08-$i-GffCompare-lncRNA.txt 03-$i-mapped_classification.filtered_lite_classification.txt | awk '{OFS="\t"} $2 ~ /Hap2/ {print $1, $2, $7}' > 09-$i-lncRNA-Hap2.txt
+grep -wFf 09-$i-GffCompare-lncRNA.txt 03-$i-mapped_classification.filtered_lite_classification.txt | awk '{OFS="\t"} $2 ~ /Hap2/ {print $1, $2, $7}' > 09-$i-lncRNA-Hap2.txt
 done
 
 # Find out how many lncRNA genes
@@ -50,16 +46,16 @@ cut -d'.' -f1,2 09-$i-lncRNA-Hap2.txt | sort -u | wc -l
 # Make fasta of predicted lncRNA genes
 for i in A4 A5 G1 SL1
 do
- seqkit grep -n -f 08-$i-GffCompare-lncRNA.txt \
+ seqkit grep -n -f 09-$i-GffCompare-lncRNA.txt \
     01-$i-illumina-filtered-simple.fasta \
-    > 88-$i-GffCompare-lncRNA-IDs.fasta
+    > 10-$i-GffCompare-lncRNA.fasta
 done
 
 # Put strain name in header
-sed '/^>/ s/$/-suffix/' input.fasta > output.fasta
+sed '/^>/ s/$/-suffix/' input.fasta > output.fasta (11)
 
 # Merge Fasta files together
-cat 888* > 11-lncRNA.fa
+cat 11* > 12-lncRNA.fa
 
 # cd-hit to cluster
-cd-hit-est -i 11-lncRNA.fa -o 111-lncRNA.fasta  -c 0.90 -n 10 -T 8 -M 0
+cd-hit-est -i 12-lncRNA.fasta -o 13-Clustered-lncRNA.fasta -c 0.98 -aS 0.9 -aL 0.9 -G 0 -g 1 -T 16 -M 0
